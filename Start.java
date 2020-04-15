c8e09945d09d5f5626059fcd222eeba4119f9cda
yhks <1725761758@qq.com>
2020-04-15 15:42:59 +0800

    注释

--- Start.java
+++ Start.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Start {
 	public static void main(String[] args) {
 		// 启动时调用初始化方法
+		// gui
 		SpringApplication.run(Start.class, args);
 	}
 }
